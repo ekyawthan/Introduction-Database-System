5c5dd3e4ddb2c3344ec822766d2a4a659312f239
Kyaw Mong <ekyawthan@gmail.com>
2014-05-23 17:23:18 -0400

    add database model

---
+++ proj3/db.sql
@@ -0,0 +1,64 @@
+CREATE TABLE CD (
+        title VARCHAR(140) NOT NULL,
+        year INT NOT NULL,
+        type CHAR(10),
+        PRIMARY KEY(title, year)
+);
+
+CREATE TABLE Song(
+        name VARCHAR(140),
+        artist VARCHAR(100),
+        trackNo INT,
+        title VARCHAR(140) NOT NULL,
+        year INT NOT NULL,
+        FOREIGN KEY (title, year) REFERENCES CD(title, year)
+);
+
+CREATE TABLE Producer(
+        name VARCHAR(140) NOT NULL,
+        address VARCHAR(200),
+        PRIMARY KEY(name)
+);
+CREATE TABLE Supplier(
+        name VARCHAR(140) NOT NULL,
+        address VARCHAR(200),
+        PRIMARY KEY(name)
+);
+
+CREATE TABLE Customer(
+        ssn INT NOT NULL,
+        name VARCHAR(140),
+        phoneNo INT,
+        PRIMARY KEY(ssn)
+);
+
+CREATE TABLE VIP(
+        ssn INT NOT NULL,
+        FOREIGN KEY(ssn) REFERENCES Customer(ssn),
+        start DATE,
+        discount INT
+);
+
+CREATE TABLE CD_Produced_By(
+        title VARCHAR(140) NOT NULL,
+        year INT NOT NULL,
+        FOREIGN KEY(title, year) REFERENCES CD(title, year),
+        name VARCHAR(140) NOT NULL,
+        FOREIGN KEY(name) REFERENCES Producer(name)
+
+);
+CREATE TABLE CD_Supply_By(
+        title VARCHAR(140) NOT NULL,
+        year INT NOT NULL,
+        FOREIGN KEY(title, year) REFERENCES CD(title, year),
+        name VARCHAR(140) NOT NULL,
+        FOREIGN KEY(name) REFERENCES Supplier(name)
+);
+
+CREATE TABLE Rent(
+        title VARCHAR(140) NOT NULL,
+        year INT NOT NULL,
+        FOREIGN KEY(title, year) REFERENCES CD(title, year),
+        ssn INT NOT NULL,
+        FOREIGN KEY(ssn) REFERENCES Customer(ssn)
+);
