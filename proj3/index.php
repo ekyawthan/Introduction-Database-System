80df2d0e9976eab901843f1c922b935e81988db7
Kyaw Mong <ekyawthan@gmail.com>
2014-05-23 17:03:45 -0400

    initial commit

---
+++ proj3/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Project3: Index</title>
+	<link rel="stylesheet" href="">
+</head>
+<style type="text/css" media="screen">
+body{
+	width:900px;
+	margin: 0 auto;
+	background: #F0E6E6;
+}
+	
+</style>
+<body>
+<form action="#" method="get" accept-charset="utf-8">
+	<div>
+		<label>Name:</label>
+		<input type="text" name="name" value="name" placeholder="Name of Producer">		
+	</div>
+	<div>
+		<label>Address: <input type="text/submit/hidden/button/etc" name="address" value="" placeholder="Address"></label>
+	</div>
+
+
+</form>
+
+	
+</body>
+</html>
